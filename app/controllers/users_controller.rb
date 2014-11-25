e2fe9555692d954b7a3a2ba59066abda4f240c8c
jake seltz <seltz.jake@gmail.com>
2014-11-25 12:33:24 -0600

    shows users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,12 @@ class UsersController < ApplicationController
 
   end
 
+  def show
+
+  	@user = User.find(params[:id])
+  end
+
+
   def create
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
   root 'users#index'
-  
-
-  get 'users/index'
-
-  get 'users/new'
-
-  get 'users/destroy'
-
-  get 'users/update'
 
 resources :users
 end
